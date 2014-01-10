62d6b3cfa56dcecd5008b19a1d79c92ed31f9f3a
Ingo93 <ingo.ruehl@mni.thm.de>
2014-01-10 13:36:20 +0100

    Signed-off-by: Ingo93 <ingo.ruehl@mni.thm.de>

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var showTable = function(name){
+	if(document.getElementById(name).style.display=="none")
+	document.getElementById(name).style.display="table";
+	else document.getElementById(name).style.display="none";
+}
